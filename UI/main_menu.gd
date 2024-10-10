aae49b64ecd70740c136eabf7a32177f8319fc56
enchantmenttable <59225760+enchantmenttable@users.noreply.github.com>
2024-10-10 10:57:41 +0700

    export runnable

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -5,6 +5,9 @@ extends CanvasLayer
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://levels/stage_select.tscn")
 
+func _ready():
+	_init_save_data()
+
 func _init_save_data():
 	var data_control = SaveData.new()
 	if data_control.load_data() == null:
